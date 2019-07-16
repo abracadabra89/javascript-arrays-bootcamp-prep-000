2a52f0357a4bb44bdc2aa115fff2a84d148af3e4
anya litvinova <anya.s.litvinova@gmail.com>
2019-07-16 21:28:03 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -11,3 +11,6 @@ function destructivelyAddElementToBeginningOfArray(array, element){
 function accessElementInArray(array, index){
   return array[index];
 }
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  return array.shift()
+}
