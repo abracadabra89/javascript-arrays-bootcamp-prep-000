acc42a56908b17ca94ff4b65c2d356a9906a5e32
anya litvinova <anya.s.litvinova@gmail.com>
2019-07-16 22:24:10 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -6,8 +6,8 @@ function addElementToBeginningOfArray(array, element){
 }
 
 function addElementToEndOfArray(array, element){
-   let newArr = array.push(element);
-   return newArr;
+   array = [...element, array];
+   return array
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element){
