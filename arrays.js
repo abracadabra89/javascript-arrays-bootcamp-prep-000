e5a290a35590f9d0a9700fbd51c75c25d6fb51e2
anya litvinova <anya.s.litvinova@gmail.com>
2019-07-16 22:32:00 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -6,7 +6,7 @@ function addElementToBeginningOfArray(array, element){
 }
 
 function addElementToEndOfArray(array, element){
-   array = [array, ...element];
+   array = array[ array.length ] = element;
    return array
 }
 
